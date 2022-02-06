8b8f3926593d7375cefa7fdd4772765dd98b1ed7
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-02-07 00:00:11 +0200

    fix revert SWC ID

--- server/exports/contracts.js
+++ server/exports/contracts.js
@@ -1,3 +1,4 @@
-const ANS_CONTRACT_ID = "eU14PhsO4u8a_mQktlq7EF33QmRjtHgqZU7ob4fCRYc";
+const ANS_CONTRACT_ID = "M6kPivAcoYAqM71xKhxq9487OWycjMiEsdHIOXds0B4";
 
+// eU14PhsO4u8a_mQktlq7EF33QmRjtHgqZU7ob4fCRYc
 module.exports = { ANS_CONTRACT_ID };
