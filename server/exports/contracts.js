dcfdbf775db9911f9985f520154840fbaced973f
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-03-12 16:30:19 +0200

    feat: testnet SWC

--- server/exports/contracts.js
+++ server/exports/contracts.js
@@ -1,4 +1,5 @@
-const ANS_CONTRACT_ID = "M6kPivAcoYAqM71xKhxq9487OWycjMiEsdHIOXds0B4";
+const ANS_CONTRACT_ID = "HrPi8hFc7M5dbrtlELfTKwPr53RRrDBgXGdDkp0h-j4";
 
 // eU14PhsO4u8a_mQktlq7EF33QmRjtHgqZU7ob4fCRYc
+// M6kPivAcoYAqM71xKhxq9487OWycjMiEsdHIOXds0B4
 module.exports = { ANS_CONTRACT_ID };
