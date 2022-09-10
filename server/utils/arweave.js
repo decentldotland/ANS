3ee5e8964a264be574feff0b0df62bd25b3f2500
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-09-10 15:48:14 +0300

    feat: fetch block by height id

--- server/utils/arweave.js
+++ server/utils/arweave.js
@@ -1,4 +1,5 @@
 import Arweave from "arweave";
+import axios from "axios";
 import { SmartWeaveNodeFactory } from "redstone-smartweave";
 
 export const arweave = Arweave.init({
@@ -10,3 +11,34 @@ export const arweave = Arweave.init({
 });
 
 export const smartweave = SmartWeaveNodeFactory.memCached(arweave);
+
+export async function getBlockTimestampById(block_id) {
+  try {
+    const q = {
+      query: `query {
+    blocks(height: {
+      min: ${block_id},
+      max: ${block_id}
+    }) {
+      edges {
+        cursor
+        node {
+            id
+            timestamp
+            height
+            previous  
+        }
+      }
+    }
+  }`,
+    };
+    const response = await axios.post("https://arweave.net/graphql", q, {
+      headers: { "Content-Type": "application/json" },
+    });
+    const gateway_res = response.data.data.blocks.edges;
+    const res = gateway_res[0].node.timestamp;
+    return res;
+  } catch (error) {
+    return null;
+  }
+}
